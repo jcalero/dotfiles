bcc459a11e66acabd2fbd2fba677b478c18d5c3a
Jakob Calero <jakob.calero@gmail.com>
2014-08-25 10:54:47 +0100

    Phpstorm 8 config, updated deploy script
    Deploy script is now a little more intelligent

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-B=$(dirname $0)
-ln -s $B/.bash_profile ~/.bash_profile
-ln -s $B/.bashrc ~/.bashrc
-ln -s $B/.bash_aliases ~/.bash_aliases
-ln -s $B/.bash_prompt ~/.bash_prompt
-ln -s $B/.gitconfig ~/.gitconfig
-ln -s $B/.gitignore ~/.gitignore
+B="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
+## Home scripts
+ln -sfv $B/.bash_profile ~/.bash_profile
+ln -sfv $B/.bashrc ~/.bashrc
+ln -sfv $B/.bash_aliases ~/.bash_aliases
+ln -sfv $B/.bash_prompt ~/.bash_prompt
+
+
+## Git scripts
+ln -sfv $B/.gitconfig ~/.gitconfig
+ln -sfv $B/.gitignore ~/.gitignore
+
+
+## Custom scripts
 mkdir -p ~/scripts
+ln -sfv $B/scripts/gitcleanup.sh ~/scripts/
 
-ln -s ../$B/scripts/gitcleanup.sh ~/scripts/
 
-#find $B/scripts -maxdepth 1 -type f -printf "%f\n" -exec \
-#  echo {} \;
-#  ln -s $B/scripts/{} ~/scripts/ \;
+## PhpStorm 8
+if [ -d ~/.WebIde80 ]; then
+  if [ -d ~/.WebIde80/config ]; then
+    mv ~/.WebIde80/config ~/.WebIde80/config-backup
+  fi
+  ln -sfv $B/phpstorm8/config ~/.WebIde80/
+fi
 
+
+## Git aware prompt
 mkdir -p ~/.bash
 cd ~/.bash
-git clone https://github.com/jimeh/git-aware-prompt
+if [ ! -d git-aware-prompt ]; then
+  git clone https://github.com/jimeh/git-aware-prompt
+fi
